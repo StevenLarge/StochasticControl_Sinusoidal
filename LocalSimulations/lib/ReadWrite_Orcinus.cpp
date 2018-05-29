2c286727aebecdccd2c28050a50f57bb38db0143
Steven Large <stevelarge7@gmail.com>
2018-05-29 14:41:54 -0700

    Added C++ files for Orcinus sumbission

---
+++ LocalSimulations/lib/ReadWrite_Orcinus.cpp
@@ -0,0 +1,143 @@
+/* This C++ file contains the Protocol read/write routines */
+
+//#include "/Users/stevelarge/Research/DiscreteControl/LinkedCode_CPP/lib/DiscreteControl.h" 			//This is the Discrete control header file, containing function prototypes
+
+#include "include/ReadWrite.h"
+
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <cmath>
+#include <string>
+#include <ctime>
+
+using namespace std;
+
+
+void ReadParameters(double * dt, double * mass, double * DampingVal, double * beta, double * TrapStrength, double * A, double * CPDist){
+
+	std::ifstream ReadFile;
+
+	ReadFile.open("Parameters.dat");
+
+	ReadFile >> *DampingVal;
+	ReadFile >> *dt;
+	ReadFile >> *beta;
+	ReadFile >> *TrapStrength;
+	ReadFile >> *mass;
+	ReadFile >> *A;
+	ReadFile >> *CPDist;
+
+	ReadFile.close();
+
+}
+
+void ReadVelocityVariance(double * VelVar){
+
+	std::ifstream ReadFile;
+
+	ReadFile.open("Parameters.dat");
+
+	double Dummy;
+
+	ReadFile >> Dummy;
+	ReadFile >> Dummy;
+	ReadFile >> Dummy;
+	ReadFile >> Dummy;
+	ReadFile >> Dummy;
+	ReadFile >> Dummy;
+	ReadFile >> Dummy;
+	ReadFile >> *VelVar;
+
+	ReadFile.close();
+
+}
+
+void ReadWriteName(string Filename){
+
+	std::ifstream ReadFile;
+
+	ReadFile.open("Parameters.dat");
+
+	double Dummy;
+
+	ReadFile >> Dummy;
+	ReadFile >> Dummy;
+	ReadFile >> Dummy;
+	ReadFile >> Dummy;
+	ReadFile >> Dummy;
+	ReadFile >> Dummy;
+	ReadFile >> Dummy;
+	ReadFile >> Dummy;
+
+	ReadFile >> Filename;
+
+	ReadFile.close();
+
+	//cout << "Filename --> " << Filename << "\n\n";
+
+}
+
+
+void ReadCPDist(double * CPDist){
+
+	std::ifstream ReadFile;
+
+	double Dummy;
+
+	ReadFile.open("Parameters.dat");
+
+	ReadFile >> Dummy;
+	ReadFile >> Dummy;
+	ReadFile >> Dummy;
+	ReadFile >> Dummy;
+	ReadFile >> Dummy;
+	ReadFile >> Dummy;
+
+	ReadFile >> *CPDist;
+
+}
+
+
+void WriteWorkData(string WriteName, double * ProtocolDuration, double * WorkVals, int NumberDurations){
+
+	std::ofstream WriteFile;
+
+	WriteFile.open(WriteName);
+
+	//for(int k = 0 ; k < NumberDurations ; k ++){
+	//	WriteFile << std::to_string(ProtocolDuration[k]) << "\t" << std::to_string(WorkVals[k]) << "\n";
+	//}
+
+	WriteFile.close();
+
+}
+
+void WriteFrictionData(string WriteName, double * CPVals, double * Friction, double * OptVel, int ArraySize){
+
+	std::ofstream WriteFile;
+
+	WriteFile.open(WriteName);
+
+	//for(int k = 0 ; k < ArraySize ; k++){
+	//	WriteFile << std::to_string(CPVals[k]) << "\t" << std::to_string(Friction[k]) << "\t" << std::to_string(OptVel[k]) << "\n";
+	//}
+
+	WriteFile.close();
+
+}
+
+void WriteRandomProtocol(string WriteName, double * OptVel, double * CPVals, int ArrayLength){
+
+	std::ofstream WriteFile;
+
+	WriteFile.open(WriteName);
+
+	//for(int k = 0 ; k < ArrayLength ; k++){
+	//	WriteFile << std::to_string(CPVals[k]) << "\t" << std::to_string(OptVel[k]) << "\n";
+	//}
+
+	WriteFile.close();
+}
+
+
---
+++ src/StochasticEnsemble_Orcinus.cpp
@@ -0,0 +1,96 @@
+/* This is a C++ driver code to propogate an ensemble of stochastic protocols along a sinusoidal landscape, confined by a harmonic trap */
+
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <cmath>
+#include <string>
+#include <ctime>
+
+#include "include/FrictionCalculator.h"
+#include "include/StochasticPropogator.h"
+#include "include/ReadWrite.h"
+
+using namespace std;
+
+
+int main(){
+
+	string WriteName = "WorkData/WorkTrend.dat";
+	string WriteNameFriction = "WorkData/Friction.dat";
+
+	double dX = 0.005;
+	//int PeriodLength = int(2.0*3.14159/dX);
+	int PeriodLength = int(2.0/dX);
+
+	double * OptVel;
+	double * CPVals;
+	double * Friction;
+	OptVel = new double [PeriodLength];
+	CPVals = new double [PeriodLength];
+	Friction = new double [PeriodLength];
+
+	int OuterIterations = 100;// 20000;
+	int InnerIterations = 1;
+	//double VelVar = 1;
+	double VelVar;
+	double * VelVarPtr = &VelVar;
+	ReadVelocityVariance(VelVarPtr);
+
+	cout << "Velocity Variance --> " << std::to_string(VelVar) << "\n";
+
+	//double ProtocolDuration [] = {0.5,1,2,4,8,16,32,64,128,256,512};//,1024};//,2048};
+	double ProtocolDuration [] = {0.031,0.063,0.125,0.25};
+	//double ProtocolDuration [] = {1,4,16};
+	int NumDurations = 4;
+	//int NumDurations = 11;//11;//12;
+	//int NumDurations = 3;
+
+	//double ProtocolDuration [] = {1};
+	//int NumDurations = 1;
+
+	double MeanVel;
+
+	double CPDist;
+	double * CPDistPtr = &CPDist;	
+	//double CPDist = 2*3.14159;
+	ReadCPDist(CPDistPtr);
+
+	double * WorkArray;
+	WorkArray = new double [NumDurations];
+
+	ZulkowskiFriction(OptVel,CPVals,Friction,dX,PeriodLength); 				//Calculate the friction over a period and the optimal velocity for a 1 second protocol
+
+	//WriteFrictionData(WriteNameFriction,CPVals,Friction,OptVel,PeriodLength);
+
+	for(int k = 0 ; k < NumDurations ; k++){
+
+		MeanVel = CPDist/ProtocolDuration[k];
+
+		WorkArray[k] = Driver(ProtocolDuration[k],CPDist,OuterIterations,InnerIterations,VelVar,MeanVel,OptVel,CPVals,PeriodLength); 	
+		//WorkArray[k] = ConstantDriver(ProtocolDuration[k],OuterIterations,InnerIterations,MeanVel,VelVar); 					//Driver for a constant velocity protocol
+
+		cout << "\t\t-----\n";
+
+	}
+
+	//WriteWorkData(WriteName,ProtocolDuration,WorkArray,NumDurations);
+
+	/* ----- WriteWorkData Routine ----- */
+
+	std::ofstream WriteFile;
+	WriteFile.open(WriteName);
+	for(int k = 0 ; k < NumberDurations ; k ++){
+		WriteFile << std::to_string(ProtocolDuration[k]) << "\t" << std::to_string(WorkVals[k]) << "\n";
+	}
+	WriteFile.close();
+
+	/* ----- --------------------- ----- */
+
+
+
+}
+
+
+
+
